5439e406300e6ba1c42a8f2a3d1abc8ece98c33c
khadi97 <khadisha.mussakhan@gmail.com>
2025-04-25 05:23:56 +0500

    Добавлена пагинация и поиск по заголовку блога

--- server/pages/router.js
+++ server/pages/router.js
@@ -5,23 +5,41 @@ const Categories = require('../Categories/Categories')
 const User = require('../auth/User')
 const Blog = require('../Blogs/Blog')
 
-router.get('/', async(req, res) => {
-    const options = {};
-    const { catId } = req.query;
-    let selectedCategory = null; 
 
-    // Если catId передан, фильтруем блоги по этой категории
+router.get('/', async (req, res) => {
+    const options = {};
+    const { catId, search, page = 0 } = req.query;
+
+    // Для пагинаций
+    const limit = 3;
+    const currentPage = parseInt(page) || 0;
+
+    let selectedCategory = null;
+
+    // Поиск по категории
     if (catId) {
         const category = await Categories.findOne({ key: catId });
         if (category) {
-            options.category = category._id; 
-            selectedCategory = catId;  // Устанавливаем выбранную категорию
+            options.category = category._id;
+            selectedCategory = catId;
+            res.locals.category = catId;
         }
     }
 
+    // Поиск по заголовку
+    if (search && search.trim().length > 0) {
+        options.$or = [
+            { title: new RegExp(search, 'i') }
+        ];
+        res.locals.search = search;
+    }
+
+    // Подсчет количества отфильтрованных блогов
+    const totalBlogs = await Blog.countDocuments(options)
+
+    const blogs = await Blog.find(options) .limit(limit).skip(currentPage * limit).populate('category').populate('author')
     const allCategories = await Categories.find()
-    const blogs = await Blog.find(options).populate('category').populate('author')
-    res.render('index', {categories: allCategories, user: req.user ? req.user : {}, blogs, selectedCategory}) //при рендеринге каждой страницы отправляю юзера
+    res.render('index', {categories: allCategories, blogs, user: req.user || {}, selectedCategory, pages: Math.ceil(totalBlogs / limit),currentPage});
 });
 
 
