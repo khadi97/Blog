bbb19faa531af26e7c9d2cb5dbad24b524545f2b
khadi97 <khadisha.mussakhan@gmail.com>
2025-05-09 03:52:16 +0500

    Дополнила некоторые моменты

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/blog', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true}).then(() => {
-    console.log('Connected to MongoDB');}).catch((e) => {
-        console.error('MongoDB connection error:', e);
-    });
+mongoose.connect('mongodb://localhost:27017/blog')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error('MongoDB connection error:', e);
+  });
